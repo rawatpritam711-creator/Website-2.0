a9953f0b9cd737ae83089b152e6fe82cf2f6bf88
rawatpritam711-creator <rawatpritam711@gmail.com>
2026-05-01 20:14:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// Main Timeline
 const tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".scroll-container",
         start: "top top",
         end: "bottom bottom",
-        scrub: 1, // Smooth scrolling effect
+        scrub: 1.5, // Thoda extra smooth kiya hai
     }
 });
 
-// Earth zoom and move animation
-tl.to(".planet-wrapper", {
-    scale: 1.8,
-    x: "30%", // Move to right side
-    y: "-20%",
-    duration: 2
+// Video ki tarah transition
+tl.to(".earth-sphere", {
+    scale: 2.2,
+    x: "35%", // Earth right side mein zoom hogi
+    y: "-15%",
+    duration: 3
 })
 .to(".hero .content", {
     opacity: 0,
-    y: -50,
-    duration: 1
-}, 0) // Starts at the same time as earth animation
+    y: -100,
+    duration: 1.5
+}, 0)
+.from(".info-card", {
+    x: -100,
+    opacity: 0,
+    duration: 1.5
+}, 1.5);
